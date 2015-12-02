d2934d50faf5b936ac96e3f1a7cc6d6162573e0c
bisneu <nicolas.nalpon@gmail.com>
2015-12-02 23:31:09 +0100

    création de deux bloc sur le fichier

--- tfs_write.c
+++ tfs_write.c
@@ -4,8 +4,10 @@
 
 int main(int argc, char *argv[]){
 	FILE *fichier = fopen(argv[1], "w");
-	uint32_t c[256]={0};
+	uint32_t c[256]={1,2,3,4};
 	fwrite(c, sizeof(uint32_t), 256, fichier);
+	uint32_t cb[256]={1};
+	fwrite(cb, sizeof(uint32_t), 256, fichier);
 	fclose(fichier);
 	return 0;
 }
