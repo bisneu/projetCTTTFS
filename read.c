1dc073c59591e2882664834e11a628636c500923
Ozukami <arthur.poisson@ymail.com>
2015-12-03 03:07:34 +0100

    read_physical_block + write_physical_block :)

--- read.c
+++ read.c
@@ -15,23 +15,19 @@ struct disk_id{
 
 typedef struct block block;
 struct block{
-	int block_id;
 	int *block_block;
 };
 
 error write_physical_block(disk_id id,block b, uint32_t num){
 	error rep;
 	rep.error_id = 0;
-	FILE *disk =NULL;
-	disk = fopen(id.disk_name,"w");
+	FILE *disk = fopen(id.disk_name,"r+");
 	if(disk==NULL){
 		rep.error_desc = "Disk introuvable";
 		rep.error_id = 1;
 		return rep;
 	}
-	printf("%ld\n", ftell(disk));
 	fseek(disk,(num*1024),SEEK_SET);
-	printf("%ld\n", ftell(disk));
 	fwrite(b.block_block, sizeof(uint32_t), 256, disk);
 	fclose(disk);
 	return rep;
@@ -40,8 +36,7 @@ error write_physical_block(disk_id id,block b, uint32_t num){
 error read_physical_block(disk_id id, block b, uint32_t num){
 	error rep;
 	rep.error_id = 0;
-	FILE *disk = NULL;
-	disk = fopen(id.disk_name, "r");
+	FILE *disk = fopen(id.disk_name, "r");
 	if(disk==NULL){
 		rep.error_desc = "Disk introuvable";
 		rep.error_id = 1;
@@ -54,63 +49,47 @@ error read_physical_block(disk_id id, block b, uint32_t num){
 	return rep;
 }
 
-int display(disk_id id, block b, uint32_t num){
-	FILE *disk = NULL;
-	disk = fopen(id.disk_name, "r");
-	uint32_t c[256];
-	fread(c, sizeof(uint32_t), 1024, disk);
+int display(disk_id id, uint32_t nb_block){
+	FILE *disk = fopen(id.disk_name, "r");
+	int nb_elem = nb_block*256;
+	uint32_t *c = NULL;
+	c = malloc(nb_block*256*4);
+	fread(c, sizeof(uint32_t), nb_elem, disk);
 	fclose(disk);
 	int i=0;
-	printf("| ");
-	for(i=0; i<256; i++){
+	printf("\n| ");
+	for(i=0; i<nb_elem; i++){
 		if(i%4 == 0 && i!=0){printf(" | ");}
+		if(i%256 == 0 && i!=0){printf("\n");}
 		if(i%32 == 0 && i!=0){printf("\n| ");}
 		printf("%d", c[i]);
 	}
-	printf(" |\n");
+	printf(" |\n\n");
 	return 0;
 }
 
-int main(){
-	printf("Execution en cours...\n");
-		block monblock;
-		monblock.block_block = malloc(1024);
-		monblock.block_id = 1;
-		printf("AVANT read_physical_block\n");
-		printf("block_block[0] = %d \n",monblock.block_block[0]);
-		printf("block_block[1] = %d \n",monblock.block_block[1]);
-		printf("block_block[2] = %d \n",monblock.block_block[2]);
-		printf("block_block[3] = %d \n",monblock.block_block[3]);
-		disk_id mondisque;
-		mondisque.disk_name = "tabite.txt";
-		error check = read_physical_block(mondisque, monblock, 0);
-		if(check.error_id != 0)
-			printf("ERREUR READ\n");
-		printf("\n");
-		printf("APRES read_physical_block\n");
-		printf("block_block[0] = %d \n",monblock.block_block[0]);
-		printf("block_block[1] = %d \n",monblock.block_block[1]);
-		printf("block_block[2] = %d \n",monblock.block_block[2]);
-		printf("block_block[3] = %d \n",monblock.block_block[3]);
-		display(mondisque, monblock, 0);
-		monblock.block_block[4] = 9;
-		monblock.block_block[5] = 9;
-		monblock.block_block[6] = 9;
-		monblock.block_block[7] = 9;
-		check = write_physical_block(mondisque, monblock, 0);
-		if(check.error_id != 0)
-			printf("ERREUR WRITE\n");
-		check = read_physical_block(mondisque, monblock, 0);
-		if(check.error_id != 0)
-			printf("ERREUR READ\n");
-		printf("\n");
-		printf("APRES write_physical_block\n");
-/*
-		printf("block_block[0] = %d \n",monblock.block_block[0]);
-		printf("block_block[1] = %d \n",monblock.block_block[1]);
-		printf("block_block[2] = %d \n",monblock.block_block[2]);
-		printf("block_block[3] = %d \n",monblock.block_block[3]);
-*/
-		display(mondisque, monblock, 0);
+int main(int argc, char *argv[]){
+	printf("Execution en cours...\n\n");
+	block my_block; my_block.block_block = malloc(1024);
+	disk_id my_disk; my_disk.disk_name = "tabite.txt";
+	printf("AVANT read_physical_block(my_disk, my_block, 0) : block_block[0,1,2,3] = {%d,%d,%d,%d} \n", my_block.block_block[0], my_block.block_block[1], my_block.block_block[2], my_block.block_block[3]);
+	error check = read_physical_block(my_disk, my_block, 0);
+	if(check.error_id != 0)
+		printf("--> ERREUR READ <--\n");
+	printf("\nAPRES read_physical_block(my_disk, my_block, 0) : block_block[0,1,2,3] = {%d,%d,%d,%d} \n", my_block.block_block[0], my_block.block_block[1], my_block.block_block[2], my_block.block_block[3]);
+	display(my_disk, 2);
+	printf("block_block[0,1,6,7] = {0,0,9,9}\n");
+	my_block.block_block[0] = 0; my_block.block_block[1] = 0;
+	my_block.block_block[6] = 9; my_block.block_block[7] = 9;
+	check = write_physical_block(my_disk, my_block, 1);
+	if(check.error_id != 0)
+		printf("--> ERREUR WRITE <--\n");
+	printf("\nAVANT read_physical_block(my_disk, my_block, 1) : block_block[2,3,6,7] = {%d,%d,%d,%d} \n", my_block.block_block[2], my_block.block_block[3], my_block.block_block[6], my_block.block_block[7]);
+	check = read_physical_block(my_disk, my_block, 1);
+	if(check.error_id != 0)
+		printf("--> ERREUR READ <--\n");
+	printf("\nAPRES read_physical_block(my_disk, my_block, 1) : block_block[2,3,6,7] = {%d,%d,%d,%d} \n", my_block.block_block[2], my_block.block_block[3], my_block.block_block[6], my_block.block_block[7]);
+	printf("\nAPRES write_physical_block(my_disk, my_block, 1)\n");
+	display(my_disk, 2);
 	printf("...Execution terminée.\n");
 }
