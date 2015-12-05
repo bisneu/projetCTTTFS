79a2114c91d53ac6503ee1264e7a07df31ed7911
Ozukami <arthur.poisson@ymail.com>
2015-12-06 00:17:48 +0100

    Ajout de stop_disk, write_block, read_block

--- logical_layer.c
+++ logical_layer.c
@@ -1,1 +1,43 @@
 #include "logical_layer.h"
+
+error write_block(disk_id id, block b, uint32_t num){
+    error rep;
+    rep = write_physical_block(id, b, num);
+	return rep;
+}
+
+error read_block(disk_id id, block b, uint32_t num){
+    error rep;
+    rep = read_physical_block(id, b, num);
+	return rep;
+}
+
+error start_disk(char *name, disk_id *id,){
+    error rep;
+	return rep;
+}
+
+/* 
+** Ferme le fichier qui correspond au disque et supprime tout 
+** les pointeurs de la mémoire afin de mettre un terme à la session
+*/
+error stop_disk(disk_id d){
+	error rep;
+	rep.error_id = 1;
+	if(d.disk_file!=NULL){
+		rep.error_id = fclose(d.disk_file);
+		if(rep.error_id!=0){
+			rep.error_desc = "Erreur fermeture";
+			return rep;
+		}
+		return rep;
+	}
+	rep.error_desc = "Erreur fichier null";
+	return rep;
+}
+
+error sync_disk(disk_id *id,){
+    error rep;
+	return rep;
+}
+
