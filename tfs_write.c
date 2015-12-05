04ac20357c267ad6e065e1329a527e3746586aeb
Ozukami <arthur.poisson@ymail.com>
2015-12-05 23:18:03 +0100

    2 blocks

--- tfs_write.c
+++ tfs_write.c
@@ -4,10 +4,8 @@
 
 int main(int argc, char *argv[]){
 	FILE *fichier = fopen(argv[1], "w");
-	uint32_t c[256]={1,2,3,4};
-	fwrite(c, sizeof(uint32_t), 256, fichier);
-	uint32_t cb[256]={1};
-	fwrite(cb, sizeof(uint32_t), 256, fichier);
+	uint32_t c[512]={5,8,3,6};
+	fwrite(c, sizeof(uint32_t), 512, fichier);
 	fclose(fichier);
 	return 0;
 }
