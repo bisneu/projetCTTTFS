1f7ef4271a818cf54be60f90c550d990b10e2f47
bisneu <nicolas.nalpon@gmail.com>
2016-01-04 22:25:11 +0100

    fichier racine

--- ttfs_volume.c
+++ ttfs_volume.c
@@ -8,7 +8,7 @@ void initiate_description_block(block block_zero, block b, uint32_t partition,ui
 	write_inblock(b,1,TTTFS_VOLUME_BLOCK_SIZE); // taille d'un block 
 	uint32_t taille_partition = read_inblock(1+partition,block_zero); 
 	write_inblock(b,2,taille_partition); // taille d'une partition 
-	uint32_t tmp = ((64*nbr_fic)-1)/1024; 
+	uint32_t tmp = ((64*nbr_fic))/1024; 
 	uint32_t libre = taille_partition-(tmp+2);
 	write_inblock(b,3,libre); // nombre de block libre 
 	write_inblock(b,4,(tmp+3)); // le numéro du premier block libre  
@@ -38,38 +38,59 @@ void initiate_file_table(disk_id id,block block_zero,int nbr_fic,int partition){
 	b.block_block = malloc(1024);
 	initiate_block(b,1024);
 	int i = 0 ;
+
 	while(compteur<nbr_fic){
-		for(i=0; i<15; i++){
-			write_inblock(b,0+(i*16),0);
-			write_inblock(b,1+(i*16),0);
-			write_inblock(b,2+(i*16),0);
-			write_inblock(b,3+(i*16),0);
-			write_inblock(b,4+(i*16),0);
-			write_inblock(b,5+(i*16),0);
-			write_inblock(b,6+(i*16),0);
-			write_inblock(b,7+(i*16),0);
-			write_inblock(b,8+(i*16),0);
-			write_inblock(b,9+(i*16),0);
-			write_inblock(b,10+(i*16),0);
-			write_inblock(b,11+(i*16),0);
-			write_inblock(b,12+(i*16),0);
-			write_inblock(b,13+(i*16),0);
-			write_inblock(b,14+(i*16),0);
-			compteur = compteur+1;
-			if(compteur < nbr_fic){
-				write_inblock(b,15+(i*16),compteur+1); // nombre de fichier 
+		for(i=0; i<16; i++){
+			if(compteur == 0 && i==0){
+				write_inblock(b,0+(i*16),0);
+				write_inblock(b,1+(i*16),1);
+				write_inblock(b,2+(i*16),0);
+				write_inblock(b,3+(i*16),0);
+				write_inblock(b,4+(i*16),0);
+				write_inblock(b,5+(i*16),0);
+				write_inblock(b,6+(i*16),0);
+				write_inblock(b,7+(i*16),0);
+				write_inblock(b,8+(i*16),0);
+				write_inblock(b,9+(i*16),0);
+				write_inblock(b,10+(i*16),0);
+				write_inblock(b,11+(i*16),0);
+				write_inblock(b,12+(i*16),0);
+				write_inblock(b,13+(i*16),0);
+				write_inblock(b,14+(i*16),0);
+				write_inblock(b,15+(i*16),compteur);
 			}
-			if(compteur == nbr_fic){ 
-				write_inblock(b,15+(i*16),compteur); // nombre de fichier 
-				write_block(id,b,block_partition+compteur2);
-				compteur2 = compteur2+1;
-				break;
+			else {
+				write_inblock(b,0+(i*16),0);
+				write_inblock(b,1+(i*16),0);
+				write_inblock(b,2+(i*16),0);
+				write_inblock(b,3+(i*16),0);
+				write_inblock(b,4+(i*16),0);
+				write_inblock(b,5+(i*16),0);
+				write_inblock(b,6+(i*16),0);
+				write_inblock(b,7+(i*16),0);
+				write_inblock(b,8+(i*16),0);
+				write_inblock(b,9+(i*16),0);
+				write_inblock(b,10+(i*16),0);
+				write_inblock(b,11+(i*16),0);
+				write_inblock(b,12+(i*16),0);
+				write_inblock(b,13+(i*16),0);
+				write_inblock(b,14+(i*16),0);
+				compteur = compteur+1;
+				if(compteur < nbr_fic){
+					write_inblock(b,15+(i*16),compteur+1); // nombre de fichier 
+				}
+				if(compteur == nbr_fic){ 
+					write_inblock(b,15+(i*16),compteur); // nombre de fichier 
+					write_block(id,b,block_partition+compteur2);
+					compteur2 = compteur2+1;
+					break;
+				}
+				else if(i==15){
+					write_block(id,b,block_partition+compteur2);
+					compteur2 = compteur2+1;
+					initiate_block(b,1024);
+				}
 			}
-			else if(i==14){
-				write_block(id,b,block_partition+compteur2);
-				compteur2 = compteur2+1;
-				initiate_block(b,1024);
-			}	
 		}
 	}
 	free(b.block_block);	
